1be2908e149f7e871f4d30924617499d0aa22fcf
J. Singleton <jennifer.singleton@gmail.com>
2019-01-30 15:42:08 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
 class Owner
   # code goes here
+  attr_accessor :pets
+  
+  def initialize
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def buy_cat(name)
+    Cat.new(name)
+  end
+
+  def buy_dog(name)
+    Dog.new(name)
+  end
+
+  def buy_fish(name)
+    Fish.new(name)
+  end
+  
 end
